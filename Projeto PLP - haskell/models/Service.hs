acd7a0107e850d513569a83798f8c31934226010
IgorArthur <igorart1210@gmail.com>
2022-07-06 21:06:22 -0300

    Module Name changed

--- Projeto PLP - haskell/models/Service.hs
+++ Projeto PLP - haskell/models/Service.hs
@@ -1,4 +1,4 @@
-module Serviço where
+module Service where
 
 type Id = Int
 type NomeCliente = String
@@ -6,7 +6,7 @@ type Contato = String
 type ModeloVeiculo = String
 type PlacaVeiculo = String
 
-data Serviço = Serviço
+data Service = Service
   { cod :: Id,
     nome :: NomeCliente,
     contato :: Contato,
@@ -18,7 +18,7 @@ data Serviço = Serviço
 
 criarServiço :: Id -> NomeCliente -> Contato -> ModeloVeiculo -> PlacaVeiculo -> IO()
 criarServiço id nome contato modelo placa = do
-  let serviço = Serviço {cod = id, nome = nome, contato = contato, modelo = modelo, placa = placa}
+  let serviço = Service {cod = id, nome = nome, contato = contato, modelo = modelo, placa = placa}
   file <- appendFile "serviços.txt" ("\n" ++ show serviço)
   putStrLn "Serviço cadastrado com sucesso!"
 
